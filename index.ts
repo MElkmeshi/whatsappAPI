e937403109ea3f7da5d1e8ac6bcb3e39038de1c5
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-09-28 16:28:56 +0300

    make it better

--- index.ts
+++ index.ts
@@ -13,14 +13,64 @@ import { Boom } from "@hapi/boom";
 import { rimraf } from "rimraf";
 import { join } from "path";
 import fileUpload from "express-fileupload";
+import WebSocket from "ws";
+import http from "http";
+import QRCode from "qrcode";
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const userConnections: Record<string, { ws: WebSocket }> = {};
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    //@ts-ignore
+    const data = JSON.parse(message);
+    console.log(data.type);
+    if (data.type === "set-username") {
+      const username = data.username;
+      userConnections[username] = { ws };
+      console.log(qrs);
+      ws.send(
+        JSON.stringify({
+          type: "qr-code",
+          message: qrs[username],
+        })
+      );
+    }
+  });
+
+  ws.on("close", () => {
+    for (const username in userConnections) {
+      if (userConnections[username].ws === ws) {
+        delete userConnections[username];
+        break;
+      }
+    }
+  });
+});
 let isListening = false;
 const PORT = process.env.PORT || 3000;
 interface IGroup {
   id: string;
   name: string;
 }
+const qrs: Record<string, string> = {};
 const baileyGenerateImage = async (base64: string, name = "qr.png") => {
+  const username = name.replace(".qr.png", "");
+  QRCode.toDataURL(base64).then((url) => {
+    qrs[username] = url;
+  });
+  if (userConnections[username]) {
+    QRCode.toDataURL(base64).then((url) => {
+      userConnections[username].ws.send(
+        JSON.stringify({
+          type: "qr-code",
+          message: url,
+        })
+      );
+    });
+  }
   const PATH_QR = `${process.cwd()}/${name}`;
   let qr_svg = qr.image(base64, { type: "png", margin: 4 });
 
@@ -86,9 +136,6 @@ class BaileysProvider {
           console.log("ready");
           if (!isListening) {
             isListening = true;
-            app.listen(PORT, () => {
-              console.log(`Example app live in http://localhost:${PORT}`);
-            });
           }
           this.initBusEvents(sock);
         }
@@ -139,61 +186,38 @@ class BaileysProvider {
     this.mysock = _sock;
   };
 }
-let bailey = new BaileysProvider("melkmeshi");
+let bailey: Record<string, BaileysProvider> = {};
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
+const checkusername = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (bailey[req.body.name]) {
+    next();
+  } else {
+    res.send({ message: "User not found" });
+  }
+};
 
-app.get("/", (req, res) => {
-  if (bailey.mysock) {
-    bailey.sendMessageWTyping({ text: "hi" }, "218910441322@s.whatsapp.net");
-    res.send({ message: "sent" });
-  } else {
-    console.log("Sock is not avalible");
-    res.send({ message: "Sock is not avalible" });
-  }
-});
-app.get("/allgroups", async (req, res) => {
-  if (bailey.mysock) {
-    const data = await bailey.mysock?.groupFetchAllParticipating();
-    res.send({ data });
-  } else {
-    console.log("Sock is not avalible");
-    res.send({ message: "Sock is not avalible" });
-  }
-});
-app.get("/getgroupid/:name", async (req, res) => {
-  if (bailey.mysock) {
-    const data = await bailey.mysock?.groupFetchAllParticipating();
-    let id = "";
-    for (const [key, value] of Object.entries(data)) {
-      if (
-        value.subject.trim().toLowerCase() ==
-        req.params.name.trim().toLowerCase()
-      ) {
-        console.log(key);
-        id = key;
-      }
-    }
-    res.send({ id });
-  } else {
-    res.send({ message: "Sock is not avalible" });
-  }
-});
-app.get("/getgroups", async (req, res) => {
-  if (bailey.mysock) {
-    const data = await bailey.mysock?.groupFetchAllParticipating();
-    const groups: IGroup[] = [];
-    for (const [key, value] of Object.entries(data)) {
-      groups.push({ name: value.subject, id: key });
-    }
-    res.send(groups);
-  } else {
-    res.send({ message: "Sock is not avalible" });
-  }
-});
-app.post("/", async (req, res) => {
-  if (!bailey.mysock) {
+// app.get("/", (req, res) => {
+//   if (bailey.mysock) {
+//     const username = req.body.username;
+//     bailey[username].sendMessageWTyping(
+//       { text: "hi" },
+//       "218910441322@s.whatsapp.net"
+//     );
+//     res.send({ message: "sent" });
+//   } else {
+//     console.log("Sock is not avalible");
+//     res.send({ message: "Sock is not avalible" });
+//   }
+// });
+app.post("/", checkusername, async (req, res) => {
+  const username = req.body.username;
+  if (!bailey[username].mysock) {
     console.log("WhatsApp connection not established.");
     return res
       .status(310)
@@ -202,6 +226,7 @@ app.post("/", async (req, res) => {
   try {
     let phoneNumber = req.body.phoneNumber || "218910441322";
     const message = req.body.message || "hi";
+    console.log(req.body);
     phoneNumber = phoneNumber
       .replaceAll("+", "")
       .replaceAll("-", "")
@@ -211,10 +236,14 @@ app.post("/", async (req, res) => {
     if (phoneNumber[3] === "0") {
       phoneNumber = phoneNumber.slice(0, 3) + phoneNumber.slice(4);
     }
-    const on = await bailey.mysock.onWhatsApp(phoneNumber);
+    const on = await bailey[username]?.mysock?.onWhatsApp(phoneNumber);
+    if (!on) {
+      res.status(404).json({ message: "User not found." });
+      return;
+    }
     if (on.length > 0) {
       console.log("Sending message to: ", phoneNumber);
-      await bailey.sendMessageWTyping(
+      await bailey[username]?.sendMessageWTyping(
         {
           text: message,
         },
@@ -232,7 +261,7 @@ app.post("/", async (req, res) => {
       .json({ message: "Internel server error.", error: String(err) });
   }
 });
-app.post("/newgroup", async (req, res) => {
+app.post("/newgroup", checkusername, async (req, res) => {
   if (!bailey.mysock) {
     console.log("WhatsApp connection not established.");
     return res
@@ -242,9 +271,14 @@ app.post("/newgroup", async (req, res) => {
   try {
     const groupName = req.body.groupName || "My test Group";
     const phoneNumber = req.body.phoneNumber || "218911779014";
-    const on = await bailey.mysock.onWhatsApp(phoneNumber);
+    const username = req.body.username;
+    const on = await bailey[username]?.mysock?.onWhatsApp(phoneNumber);
+    if (!on) {
+      res.status(404).json({ message: "User not found." });
+      return;
+    }
     if (on.length > 0) {
-      const group = await bailey.mysock.groupCreate(groupName, [
+      const group = await bailey[username]?.mysock?.groupCreate(groupName, [
         `${phoneNumber
           .replaceAll("+", "")
           .replaceAll("-", "")
@@ -263,15 +297,16 @@ app.post("/newgroup", async (req, res) => {
       .json({ message: "Internel server error.", error: String(err) });
   }
 });
-app.post("/group", async (req, res) => {
+app.post("/group", checkusername, async (req, res) => {
   try {
     const groupID = req.body.groupid;
     const message = req.body.message || "hi";
+    const username = req.body.username;
     if (!groupID) {
       res.status(400).json({ message: "Group id is required." });
       return;
     }
-    await bailey.mysock?.sendMessage(groupID, {
+    await bailey[username].mysock?.sendMessage(groupID, {
       text: message,
     });
     res.status(200).json({ message: "Message sent." });
@@ -282,17 +317,12 @@ app.post("/group", async (req, res) => {
       .json({ message: "Internel server error.", error: String(err) });
   }
 });
-app.get("/sendattchment", async (req, res) => {
-  res.sendFile(__dirname + "/sendattchment.html");
-});
-app.get("/sendgroup", async (req, res) => {
-  res.sendFile(__dirname + "/groupmessage.html");
-});
-app.post("/sendvideo", async (req, res) => {
+app.post("/sendvideo", checkusername, async (req, res) => {
   const video = req.files!.video as fileUpload.UploadedFile;
   const phoneNumber = req.body.phoneNumber + "@s.whatsapp.net";
   const caption = req.body.caption;
-  await bailey.sendMessageWTyping(
+  const username = req.body.username;
+  await bailey[username].sendMessageWTyping(
     {
       video: video.data,
       caption,
@@ -302,11 +332,12 @@ app.post("/sendvideo", async (req, res) => {
   );
   res.send("ok");
 });
-app.post("/sendimage", async (req, res) => {
+app.post("/sendimage", checkusername, async (req, res) => {
   const image = req.files!.image as fileUpload.UploadedFile;
   const phoneNumber = req.body.phoneNumber + "@s.whatsapp.net";
   const caption = req.body.caption;
-  await bailey.sendMessageWTyping(
+  const username = req.body.username;
+  await bailey[username].sendMessageWTyping(
     {
       image: image.data,
       caption: caption ?? "",
@@ -315,10 +346,11 @@ app.post("/sendimage", async (req, res) => {
   );
   res.send("ok");
 });
-app.post("/sendfile", async (req, res) => {
+app.post("/sendfile", checkusername, async (req, res) => {
   const file = req.files!.file as fileUpload.UploadedFile;
   const phoneNumber = req.body.phoneNumber + "@s.whatsapp.net";
-  await bailey.sendMessageWTyping(
+  const username = req.body.username;
+  await bailey[username].sendMessageWTyping(
     {
       document: file.data,
       mimetype: file.mimetype,
@@ -328,15 +360,77 @@ app.post("/sendfile", async (req, res) => {
   );
   res.send("ok");
 });
-app.post("/bulk", async (req, res) => {
+app.post("/bulk", checkusername, async (req, res) => {
   const message = req.body.message;
   const numbers = req.body.numbers;
+  const username = req.body.username;
   const numbersArray = numbers.split(",");
   for (const number of numbersArray) {
-    await bailey.sendMessageWTyping(
+    await bailey[username].sendMessageWTyping(
       { text: message },
       `${number}@s.whatsapp.net`
     );
   }
   res.send("ok");
+});
+app.get("/sendattchment", async (req, res) => {
+  res.sendFile(__dirname + "/sendattchment.html");
+});
+app.get("/sendgroup", async (req, res) => {
+  res.sendFile(__dirname + "/groupmessage.html");
+});
+app.get("/scan/:username", async (req, res) => {
+  const username = req.params.username;
+  if (!bailey.hasOwnProperty(username)) {
+    console.log("new user");
+    bailey[username] = new BaileysProvider(username);
+  }
+  res.sendFile(__dirname + "/scan.html");
+});
+app.get("/allgroups/:username", async (req, res) => {
+  if (bailey.mysock) {
+    const username = req.params.username;
+    const data = await bailey[username].mysock?.groupFetchAllParticipating();
+    res.send({ data });
+  } else {
+    console.log("Sock is not avalible");
+    res.send({ message: "Sock is not avalible" });
+  }
+});
+app.get("/getgroupid/:name/:username", async (req, res) => {
+  if (bailey.mysock) {
+    const username = req.params.username;
+    const data = await bailey[username].mysock?.groupFetchAllParticipating();
+    let id = "";
+    if (!data) return res.send({ message: "No groups found" });
+    for (const [key, value] of Object.entries(data)) {
+      if (
+        value.subject.trim().toLowerCase() ==
+        req.params.name.trim().toLowerCase()
+      ) {
+        console.log(key);
+        id = key;
+      }
+    }
+    res.send({ id });
+  } else {
+    res.send({ message: "Sock is not avalible" });
+  }
+});
+app.get("/getgroups/:username", async (req, res) => {
+  if (bailey.mysock) {
+    const username = req.params.username;
+    const data = await bailey[username].mysock?.groupFetchAllParticipating();
+    const groups: IGroup[] = [];
+    if (!data) return res.send({ message: "No groups found" });
+    for (const [key, value] of Object.entries(data)) {
+      groups.push({ name: value.subject, id: key });
+    }
+    res.send(groups);
+  } else {
+    res.send({ message: "Sock is not avalible" });
+  }
+});
+server.listen(PORT, () => {
+  console.log("Server is running on port 3000");
 });
